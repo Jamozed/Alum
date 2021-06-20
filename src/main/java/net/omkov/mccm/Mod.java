9e907f8c0bed8da2f82458eb994263d3ca2fdb63
Jamozed <jake@omkov.net>
2021-06-21 11:30:45 +1200

    Add code skeleton

---
+++ src/main/java/net/omkov/mccm/Mod.java
@@ -0,0 +1,16 @@
+// Mod.java
+// Entrypoint and singleton class for MCCM
+// Copyright (C) 2020, Jakob Wakeling
+// All rights reserved.
+
+package net.omkov.mccm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+/** The Mod class provides entrypoints and global data storage. */
+public class Mod implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		
+	}
+}
